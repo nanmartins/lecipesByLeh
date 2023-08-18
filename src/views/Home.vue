dfc9428695e5acb71a9eb4f0d6532b4976d001fc
renan martins <renanmartins989@gmail.com>
2023-08-18 03:34:12 +0100

    using router link at home for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <v-container class="fill-height">
     <v-responsive class="align-center text-center">
       <h1>Home</h1>
+      <router-link to="/recipes">Recipes</router-link>
       <router-view></router-view>
     </v-responsive>
   </v-container>
