a5447fb593805b9cdaada710a6aa407193a7537a
renan martins <renanmartins989@gmail.com>
2023-08-27 23:55:21 +0100

    ...

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="bg-primary mt-6 pa-0" sticky-footer>
+  <v-footer class="bg-primary mt-6 pa-0 mx-0" sticky-footer>
     <v-row
       no-gutters
       class="justify-center"
@@ -9,20 +9,20 @@
         <v-img :src="LogoHeader" max-width="220px" width="100%"></v-img>
       </v-col>
 
-      <v-row class="d-flex justify-center py-4">
+      <v-col cols="12" class="d-flex justify-center bg-details2">
         <v-btn
           v-for="icon in icons"
           :key="icon"
-          class="mx-4 text-details"
+          class="mx-4 text-primary"
           :icon="icon"
           variant="plain"
           size="small"
         ></v-btn>
-      </v-row>
+      </v-col>
 
       <v-col
         cols="12"
-        class="text-center py-2 text-primary bg-details2"
+        class="text-center py-2 text-details2 bg-primary"
       >
         {{ new Date().getFullYear() }} — <strong>LecipesByLeh</strong>
       </v-col>
