3c6ff5d653ed2ed779368d588cf5dac7f0107f51
renan martins <renanmartins989@gmail.com>
2023-09-02 06:15:32 +0100

    still need to fix the home banner

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,11 +1,21 @@
 <template>
   <v-responsive>
     <div
-      class="d-flex align-center justify-space-between px-2 px-md-4 py-4 mx-2 mx-md-4 rounded"
-      style="border: 1px solid #FFB4A2"
+      class="d-flex align-center justify-center px-2 px-md-4 py-4 mx-auto"
+      max-width="1200px"
     >
-      <h1 class="px-4">Bem vindo ao meu livro de receitas digital</h1>
-      <v-img :src="HomeImg" height="500px" width="300px"></v-img>
+      <v-col cols="4" class="text-end">
+        <h1
+          class="px-4 text-details2 text-h4 text-m font-weight-bold"
+          style="text-shadow: 1px 1px grey; line-height: normal;"
+        >
+          Bem vindos ao meu livro de receitas digital
+        </h1>
+      </v-col>
+
+      <v-col cols="4">
+        <v-img :src="HomeImg" max-height="600px" width="300px"></v-img>
+      </v-col>
     </div>
   </v-responsive>
 </template>
